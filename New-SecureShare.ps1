62b421e50853da48cbe94556db138118c94106eb
Aaron Parker <aaron@stealthpuppy.com>
2018-12-03 00:12:09 +1100

    Update New-SecureShare.ps1
    First working version of v2.0

--- New-SecureShare.ps1
+++ New-SecureShare.ps1
@@ -1,5 +1,5 @@
-# Requires -Version 2
-# Requires -RunAsAdministrator
+#Requires -Version 5
+#Requires -RunAsAdministrator
 <#PSScriptInfo
     .VERSION 2.0
     .GUID f5fcfeee-d09e-48ce-b0b7-c68e23d64f66
@@ -14,14 +14,14 @@
     .REQUIREDSCRIPTS 
     .EXTERNALSCRIPTDEPENDENCIES 
     .RELEASENOTES
-    .PRIVATEDATA 
+    .PRIVATEDATA
 #>
 <#
     .SYNOPSIS
-        Create secure shared folders for home directories / redirected folders and profiles
+        Create secure shared folders for home directories / redirected folders and profiles.
     
     .DESCRIPTION
-        Create secure shared folders for home directories / redirected folders and profiles
+        Create secure shared folders for home directories / redirected folders and profiles.
         
         Sources:
         https://support.microsoft.com/en-us/help/274443/how-to-dynamically-create-security-enhanced-redirected-folders-by-using-folder-redirection-in-windows-2000-and-in-windows-server-2003
@@ -40,75 +40,167 @@
     .OUTPUTS
 
     .PARAMETER Path
-        Specified a local path to share.
+        Specifies a local path to share.
+
+    .PARAMETER Description
+        Specifies a description for the share.
+
+    .PARAMETER CachingMode
+        Specifies the caching mode of the offline files for the SMB share. There are five caching modes:
+
+            -- None. Prevents users from storing documents and programs offline.
+            -- Manual. Allows users to identify the documents and programs they want to store offline.
+            -- Programs. Automatically stores documents and programs offline.
+            -- Documents. Automatically stores documents offline.
+            -- BranchCache. Enables BranchCache and manual caching of documents on the shared folder.
 
     .EXAMPLE
-        .\New-SecureShare.ps1 -Path "E:\Home"
+        .\New-SecureShare.ps1 -Path "E:\Home" -CachingMode Documents
 
         Description:
-        Creates a secure share for the folder E:\Home named Home.
+        Creates a secure share for the folder E:\Home named Home, with Offline Settings set to automatic.
+
+    .EXAMPLE
+        .\New-SecureShare.ps1 -Path "E:\Profiles" -Description "User roaming profiles"
+
+        Description:
+        Creates a secure share for the folder E:\Profiles named Profiles, with Offline Settings set to none and sets a custom description.
 #>
 [CmdletBinding(SupportsShouldProcess = $True, HelpUri = 'https://github.com/aaronparker/secure-shares')]
 [OutputType([System.Array])]
 Param (
     [Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True, `
             HelpMessage = 'Specify a target path for the share.')]
+    [ValidateScript( {
+            if ( -Not ((Split-Path -Path $_) | Test-Path) ) {
+                throw "Parent path $(Split-Path -Path $_) does not exist."
+            }
+            return $true
+        })]
     [Alias('FullName', 'PSPath')]
     [string] $Path,
 
-    [Parameter(Mandatory = $True, HelpMessage = 'Specify a description for the share.')]
-    [string] $Description = "User home folders"
+    [Parameter(Mandatory = $False, HelpMessage = 'Specify a description for the share.')]
+    [string] $Description = "Secure share with access-based enumeration. Created with PowerShell.",
+
+    [Parameter(Mandatory = $False, HelpMessage = 'Set the share caching mode. Use None for profile shares.')]
+    [ValidateSet('None', 'Manual', 'Documents', 'Programs', 'BranchCache')]
+    [string] $CachingMode = "None"
 )
 
+# Trust the PowerShell Gallery
 Function Install-PSGallery {
-    <# Trust the PowerShell Gallery #>
+    [CmdletBinding(SupportsShouldProcess = $True)]
+    Param()
     If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
         Write-Verbose "Trusting the repository: PSGallery"
-        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+        If ($pscmdlet.ShouldProcess("NuGet", "Installing Package Provider")) {
+            try {
+                Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208
+            }
+            catch {
+                Throw "Failed to install package provider NuGet with error $_."
+                Break
+            }
+        }
+        If ($pscmdlet.ShouldProcess("PowerShell Gallery", "Trusting PowerShell Repository")) {
+            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+        }
     }
 }
 
 # Get share name from $Path
 $share = $(Split-Path $Path -Leaf)
-
-try {
-    # Install the NTFSSecurity module from the PowerShell Gallery
-    If (!(Get-Module NTFSSecurity)) {
-        Install-PSGallery
-        Install-Module -Name NTFSSecurity -ErrorAction SilentlyContinue -Verbose
-    }
+If (Get-SmbShare -Name $share -ErrorAction SilentlyContinue) {
+    Write-Warning "$share share already exists."
+    $output = Get-SmbShare -Name $share -ErrorAction SilentlyContinue | Select-Object -First 1
+    Write-Output $output
 }
-catch {
-    Write-Error "Unable to install the module NTFSSecurity with error $_."
-    Break
-}
-finally {
-    # Create the folder
+Else {
     try {
-        If (!(Test-Path -Path $Path)) { New-Item -Path $Path -ItemType Directory }
+        # Install the NTFSSecurity module from the PowerShell Gallery
+        If (!(Get-Module NTFSSecurity)) {
+            Install-PSGallery
+            If ($pscmdlet.ShouldProcess("NTFSSecurity", "Installing module")) {
+                Install-Module -Name NTFSSecurity -ErrorAction SilentlyContinue -Verbose
+            }
+        }
     }
     catch {
-        Write-Error "Failed to create folder $Path with error $_."
+        Write-Error "Unable to install the module NTFSSecurity with error $_."
+        Break
+    }
+    finally {
+        # Create the folder
+        try {
+            If (!(Test-Path -Path $Path)) {
+                If ($pscmdlet.ShouldProcess($Path, "Creating directory")) {
+                    New-Item -Path $Path -ItemType Directory > $Null
+                }
+            }
+        }
+        catch {
+            Write-Error "Failed to create folder $Path with error $_."
+        }
+
+
+        # Clear permissions on the path so that we can re-create secure permissions
+        If ($pscmdlet.ShouldProcess($Path, "Clearing NTFS permissions")) {
+            Clear-NTFSAccess -Path $Path -DisableInheritance
+        }
+
+        # Add NTFS permissions for securely creating shares
+        # Administrators and System
+        If ($pscmdlet.ShouldProcess($Path, "Adding 'Administrators', 'System' with Full Control")) {
+            ForEach ($account in 'Administrators', 'System') {
+                $addNtfsParams = @{
+                    Path         = $Path
+                    Account      = $account
+                    AccessRights = 'FullControl'
+                }
+                Add-NTFSAccess @addNtfsParams
+            }
+        }
+
+        # Users - enable the ability to create a folder
+        If ($pscmdlet.ShouldProcess($Path, "Adding 'Users' rights to create sub-folders")) {
+            $addNtfsParams = @{
+                Path         = $Path
+                Account      = 'Users'
+                AppliesTo    = 'ThisFolderOnly'
+                AccessRights = @('CreateDirectories', 'ListDirectory', 'AppendData', 'Traverse', 'ReadAttributes')
+            }
+            Add-NTFSAccess @addNtfsParams
+        }
+
+        # Creator Owner - users then get full control on the folder they've created
+        If ($pscmdlet.ShouldProcess($Path, "Adding 'CREATOR OWNER' with Full Control on sub-folders")) {
+            $addNtfsParams = @{
+                Path         = $Path
+                Account      = 'CREATOR OWNER'
+                AppliesTo    = 'SubfoldersAndFilesOnly'
+                AccessRights = 'FullControl'
+            }
+            Add-NTFSAccess @addNtfsParams
+        }
+
+        # Share the folder with access-based enumeration
+        If ($pscmdlet.ShouldProcess($Path, "Sharing")) {
+            $newShareParams = @{
+                Name                  = $share
+                Path                  = $Path
+                FolderEnumerationMode = 'AccessBased'
+                FullAccess            = 'Administrators'
+                ChangeAccess          = 'Authenticated Users'
+                ReadAccess            = 'Everyone'
+                CachingMode           = $CachingMode
+                Description           = $Description
+            }
+            New-SMBShare @newShareParams
+        }
     }
 
-    # If the folder was created, let's set permissions and share it
-    If (Test-Path -Path $Path) {
-        Disable-NTFSAccessInheritance -Path $Path
-        Get-NTFSAccess -Path $Path -Account Users | Remove-NTFSAccess
-        Add-NTFSAccess -Path $Path -Account Users -AppliesTo ThisFolderOnly `
-            -AccessRights CreateDirectories, ListDirectory, AppendData, Traverse, ReadAttributes
-
-        # Share the folder
-        New-SMBShare –Name $share –Path $Path `
-            -FolderEnumerationMode AccessBased `
-            –FullAccess "Administrators"  `
-            -ChangeAccess "Authenticated Users" `
-            -ReadAccess "Everyone" `
-            -CachingMode Documents `
-            -Description $Description
-
-        # Return share details
-        Write-Output (Get-SmbShare -Name $share)
-    }
+    # Return share details (Get-SmbShare returns the shared folder twice)
+    $output = Get-SmbShare -Name $share -ErrorAction SilentlyContinue | Select-Object -First 1
+    Write-Output $output
 }
